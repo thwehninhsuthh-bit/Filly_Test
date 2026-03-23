6835e60bc0f5f7f0fe15cf75685076012c30f90f
thwehninhsuthh-bit <thwehninhsu.thh@gmail.com>
2026-03-23 21:07:02 +0630

    First commit: Initializing Spring Boot Project

---
+++ src/main/java/com/YouTubeSpringBoot/YouTubeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.YouTubeSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouTubeSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouTubeSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/YouTubeSpringBoot/entity/TestEntity.java
@@ -0,0 +1,15 @@
+package com.YouTubeSpringBoot.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class TestEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(length=20,nullable = false)
+    private String name;
+
+}
